5fcdea2c6df5bfadb4e23a2be608234ad48a370f
ChenYang-pro <chenyang_nn@163.com>
2022-01-24 23:42:27 +0800

    feat: introduce

---
+++ src/view/common/type.ts
@@ -0,0 +1,9 @@
+type IntroMessage = {
+  pic: Array<string>;
+  price: object;
+  goodsName: string;
+  saleInfo: string;
+  addressInfo: string;
+};
+
+export type { IntroMessage };
--- src/view/index/index.tsx
+++ src/view/index/index.tsx
@@ -2,9 +2,10 @@ import React from 'react';
 import Intro from '../introduction';
 import Commodity from '../commodity';
 import LikeLiHood from '../likelihood';
+import { IntroMessage } from '../common/type';
 
 function Index() {
-  const info = {
+  const info: IntroMessage = {
     pic: [
       'https://p2.eckwai.com/ufile/adsocial/1dc759e9-7c62-4ca8-9444-11891bdcbaa1.jpg',
       'https://p2.eckwai.com/ufile/adsocial/1dc759e9-7c62-4ca8-9444-11891bdcbaa1.jpg'
@@ -18,8 +19,8 @@ function Index() {
     addressInfo: '48小时内从北京市市辖区发货'
   };
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center">
-      <Intro information={info} />
+    <div className="w-full">
+      <Intro info={info} />
       <Commodity />
       <LikeLiHood />
     </div>
